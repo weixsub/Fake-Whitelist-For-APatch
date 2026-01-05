5d4720b4d88404e827de72bed7faf77b242849de
weixsub <136544637+weixsub@users.noreply.github.com>
2026-01-05 10:45:29 +0800

    Update monitor.c

--- monitor.c
+++ monitor.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
@@ -7,41 +6,46 @@
 int main(int argc, char** argv) {
   if (argc < 4) return 1;
 
-  signal(SIGCHLD, SIG_IGN);
+  struct sigaction sa = {
+    .sa_handler = SIG_IGN,
+    .sa_flags = SA_NOCLDWAIT
+  };
+  sigemptyset(&sa.sa_mask);
+  sigaction(SIGCHLD, &sa, NULL);
 
-  char* link = argv[0];
-  char* dir = argv[1];
-  char* script = argv[2];
-  int mode = atoi(argv[3]);
+  char* script = argv[1];
+  char* dir = argv[2];
+  char* event = argv[3];
 
   int fd = inotify_init();
   if (fd < 0) return 1;
 
-  int wd = inotify_add_watch(fd, dir, IN_CREATE | IN_MOVED_TO | IN_DELETE_SELF);
+  int wd = inotify_add_watch(
+    fd, dir, IN_CREATE | IN_MOVED_TO | IN_DELETE_SELF
+  );
   if (wd < 0) return 1;
 
-  char buf[1024];
+  char buf[4096];
 
   for (;;) {
-    int len = read(fd, buf, sizeof(buf));
-    if (len <= 0) return 1;
+    int bt = read(fd, buf, sizeof(buf));
+    if (bt <= 0) return 1;
 
     int i = 0;
-    while (i < len) {
+    while (i < bt) {
       struct inotify_event* ev = (void*)(buf + i);
 
-      if (ev->mask & IN_DELETE_SELF) {
-        unlink(link);
-        return 0;
-      }
+      int match = (event[0] == 'n' && (ev->mask & IN_CREATE)) ||
+        (event[0] == 'm' && (ev->mask & IN_MOVED_TO));
 
-      if ((mode == 0 && (ev->mask & IN_CREATE) && (ev->mask & IN_ISDIR)) ||
-        (mode == 1 && (ev->mask & IN_MOVED_TO) && (ev->mask & IN_ISDIR))) {
+      if (match && (ev->mask & IN_ISDIR)) {
         if (fork() == 0) {
-          execl("/system/bin/sh", "sh", script, dir, ev->name, (char*)NULL);
-          _exit(0);
+          execl("/system/bin/sh", "sh", script, event, dir, ev->name, (char*)NULL);
+          _exit(1);
         }
       }
+
+      if (ev->mask & IN_DELETE_SELF) return 0;
 
       i += sizeof(struct inotify_event) + ev->len;
     }
